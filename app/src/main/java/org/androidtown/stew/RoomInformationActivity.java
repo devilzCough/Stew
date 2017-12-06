0234a846128c98e54d58037f774b60080ab79b1e
jeonghyun <38jh1116@naver.com>
2017-12-07 05:41:17 +0900

    - study room information activity

--- app/src/main/java/org/androidtown/stew/RoomInformationActivity.java
+++ app/src/main/java/org/androidtown/stew/RoomInformationActivity.java
@@ -1,13 +1,27 @@
 package org.androidtown.stew;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class RoomInformationActivity extends AppCompatActivity {
 
+    String floor_order;
+    String room;
+
+    TextView txtLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_information);
+
+        txtLocation = (TextView)findViewById(R.id.txtLocation);
+        Intent receievedIntent = getIntent();
+        floor_order = receievedIntent.getStringExtra("floor");
+        room = receievedIntent.getStringExtra("room");
+
+        txtLocation.setText(floor_order+" "+room);
+
     }
 }
